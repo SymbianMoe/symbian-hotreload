659b844eb71adea7e60c7078824b5dc5092113e3
Mohamed Shawky <moe.shawky1799@icloud.com>
2022-11-16 08:20:01 +0200

    update weback.config

--- webpack.config.js
+++ webpack.config.js
@@ -43,41 +43,4 @@ const server = {
   target: 'node',
 };
 
-const client = {
-  entry: './src/client/client.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['ts-loader', 'eslint-loader'],
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  plugins: [
-    new RemovePlugin({
-      before: {
-        include: [
-          path.resolve(buildPath, 'client')
-        ]
-      },
-      watch: {
-        include: [
-          path.resolve(buildPath, 'client')
-        ]
-      }
-    })
-  ],
-  optimization: {
-    minimize: true,
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: '[contenthash].client.js',
-    path: path.resolve(buildPath, 'client'),
-  },
-};
-
-module.exports = [server, client];
+module.exports = [server];
