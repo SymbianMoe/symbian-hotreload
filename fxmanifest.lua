bb82e0ee4f6b9e909a0f0fca55fd3acdf86874b5
Mohamed Shawky <moe.shawky1799@icloud.com>
2022-11-16 08:18:28 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,10 @@
 fx_version 'bodacious'
 game 'gta5'
 
-name 'Abyu-HotReload'
-description 'A boilerplate for using Typescript in FiveM'
+name 'Symbian-HotReload'
+description 'A Hot Reload Script for FiveM'
 author 'Mohamed Shawky'
-url 'https://github.com/d0p3t/fivem-ts-boilerplate'
+url 'https://github.com/SymbianMoe/symbian-hotreload'
 
 client_script 'dist/client/*.client.js'
 server_script 'dist/server/*.server.js'
