75e6f0833430d7654c7cd38c9b0072574a89c470
Charlie <dev@charlie.fyi>
2022-11-17 03:48:41 +0000

    Formatter

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,32 +4,35 @@ import { Signale } from 'signale';
 import { config } from '../config';
 
 export function fileExistsInPath(folder: string, file: string): boolean {
-    return fs.existsSync(path.join(folder, file))
+  return fs.existsSync(path.join(folder, file));
 }
 
 export function checkIfValidResource(resource_path: string): boolean {
-    const resource_name = getLastDir(resource_path)
-    if (config.resources.paths.some(s => resource_name.includes(s) )){
+  const resource_name = getLastDir(resource_path);
+  if (config.resources.paths.some(s => resource_name.includes(s))) {
+    return false;
+  }
+  const fx_manifest = path.join(resource_path, 'fxmanifest.lua');
+  if (fileExistsInPath(resource_path, 'fxmanifest.lua')) {
+    const fx_manifest_contents = fs.readFileSync(fx_manifest, { encoding: 'utf8' }).toString();
+    const fx_version = fx_manifest_contents.search('fx_version');
+    const game_type = fx_manifest_contents.search('game');
+    if (fx_version !== -1 && game_type !== -1) {
+      return true;
+    } else {
+      print.error(`invalid resource: ${resource_name} (empty fxmanifest)`);
       return false;
     }
-    const fx_manifest = path.join(resource_path, 'fxmanifest.lua');
-    if (fileExistsInPath(resource_path, 'fxmanifest.lua')) {
-        const fx_manifest_contents = fs.readFileSync(fx_manifest, {encoding: 'utf8'}).toString();
-        const fx_version = fx_manifest_contents.search('fx_version');
-        const game_type = fx_manifest_contents.search('game');
-        if (fx_version !== -1 && game_type !== -1) {
-            return true;
-        } else {
-            print.error(`invalid resource: ${resource_name} (empty fxmanifest)`);
-            return false;
-        }
-    }
-    print.error(`invalid resource: ${resource_name} (no fxmanifest file)`);
-    return false;
+  }
+  print.error(`invalid resource: ${resource_name} (no fxmanifest file)`);
+  return false;
 }
-  
+
 export function extractResourceName(path: string): string {
-  return splitMulti(path,config.resources.paths).pop().split(config.directorySeparators[process.platform]).pop();
+  return splitMulti(path, config.resources.paths)
+    .pop()
+    .split(config.directorySeparators[process.platform])
+    .pop();
 }
 
 export function getLastDir(path: string): string {
@@ -38,26 +41,31 @@ export function getLastDir(path: string): string {
 
 export const prepareResourcePaths = (): string[] => {
   return config.resources.paths.map(resourcePath => {
-    return path.join(path.resolve(process.cwd()), 'resources', resourcePath)
+    return path.join(path.resolve(process.cwd()), 'resources', resourcePath);
   });
-}
+};
 
-export function splitMulti(str: string, separatorsArr: string[]): string[]{
+export function splitMulti(str: string, separatorsArr: string[]): string[] {
   //  Removing [] from the string (because regex doesn't support it)
-  const separators = separatorsArr.map((val)=>{
+  const separators = separatorsArr
+    .map(val => {
       val = val.split('[').join('');
       val = val.split(']').join('');
       return val;
-  }) // adding the equivalent to [] in regex to values and generates regex expression
-  .reduce((prev,curr,index,arr)=>{
-      prev += `[\x5B]${curr}]${index != arr.length-1 ? "|":"" }`;
-      return prev
-  },"")
-  
+    }) // adding the equivalent to [] in regex to values and generates regex expression
+    .reduce((prev, curr, index, arr) => {
+      prev += `[\x5B]${curr}]${index != arr.length - 1 ? '|' : ''}`;
+      return prev;
+    }, '');
+
   const tempChar = 't3mp'; //prevent short text separator in split down
   // split by regex e.g. \b(or|and)\b then trim & remove empty
-  const re = new RegExp('\.('+ separators +')', "g");
-  return str.replace(re, tempChar).split(tempChar).map(el => el.trim()).filter(el => el.length > 0);
+  const re = new RegExp('.(' + separators + ')', 'g');
+  return str
+    .replace(re, tempChar)
+    .split(tempChar)
+    .map(el => el.trim())
+    .filter(el => el.length > 0);
 }
 
 export const print = new Signale(config.logger);
--- src/utils/watcher.ts
+++ src/utils/watcher.ts
@@ -1,59 +1,52 @@
-
 import chokidar from 'chokidar';
-
-import {
-    extractResourceName,
-    checkIfValidResource,
-    print
-  } from './utils';
-
-import {Resources} from './resources';
+import { extractResourceName, checkIfValidResource, print } from './utils';
+import { Resources } from './resources';
 
 export class MyWatcher {
-    private _watcher :chokidar.FSWatcher;
-    private _initialized :boolean;
-    private _resource_path :string | string[];
-    private _resources :Resources;
-    private _options :chokidar.WatchOptions;
+  private _watcher: chokidar.FSWatcher;
+  private _initialized: boolean;
+  private _resource_path: string | string[];
+  private _resources: Resources;
+  private _options: chokidar.WatchOptions;
 
-    constructor(resources_path:any , options:chokidar.WatchOptions) {
-        this._initialized = false;
-        this._resource_path = resources_path;
-        this._options = options;
-        this._resources = new Resources();
-    }
+  constructor(resources_path: any, options: chokidar.WatchOptions) {
+    this._initialized = false;
+    this._resource_path = resources_path;
+    this._options = options;
+    this._resources = new Resources();
+  }
 
-    private onReady(): void {
-        this._initialized = true;
-        print.watch('Initial scan complete. All resources are loaded. Ready for changes')
-        print.debug('Watching for changes in', this._resource_path);
-    }
+  private onReady(): void {
+    this._initialized = true;
+    print.watch('Initial scan complete. All resources are loaded. Ready for changes');
+    print.debug('Watching for changes in', this._resource_path);
+  }
 
-    private onDirAdd(path: string) {
-        const resourceName: string = extractResourceName(path);
-        if (checkIfValidResource(path)) {
-          print.complete(`Resource "${resourceName}" is valid, starting resource..`)
-          this._resources.addResource(resourceName);
-        }
+  private onDirAdd(path: string) {
+    const resourceName: string = extractResourceName(path);
+    if (checkIfValidResource(path)) {
+      print.complete(`Resource "${resourceName}" is valid, starting resource..`);
+      this._resources.addResource(resourceName);
     }
+  }
 
-    private onDirUnlink(path: string): void {
-        this._resources.removeResource(extractResourceName(path));
-    }
+  private onDirUnlink(path: string): void {
+    this._resources.removeResource(extractResourceName(path));
+  }
 
-    private onError(error: Error): void {
-        print.error(`Watcher error: ${error}`)
-    }
+  private onError(error: Error): void {
+    print.error(`Watcher error: ${error}`);
+  }
 
-    public startWatching(): void {
-        if (this._initialized){
-            print.error(`Watcher error: Watcher is Already Started`)
-            return;
-        }
-        this._watcher = chokidar.watch(this._resource_path, this._options);
-        this._watcher.on('ready', () => this.onReady());
-        this._watcher.on('addDir', (path) => this.onDirAdd(path));
-        this._watcher.on('unlinkDir', (path) => this.onDirUnlink(path));
-        this._watcher.on('error', (err) => this.onError(err));
+  public startWatching(): void {
+    if (this._initialized) {
+      print.error(`Watcher error: Watcher is Already Started`);
+      return;
     }
+    this._watcher = chokidar.watch(this._resource_path, this._options);
+    this._watcher.on('ready', () => this.onReady());
+    this._watcher.on('addDir', path => this.onDirAdd(path));
+    this._watcher.on('unlinkDir', path => this.onDirUnlink(path));
+    this._watcher.on('error', err => this.onError(err));
+  }
 }
