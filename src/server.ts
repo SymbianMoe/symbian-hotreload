3b36fd0d2884f3ab49b2bd37d0e43cf46ab70424
Charlie <dev@charlie.fyi>
2022-11-17 03:42:15 +0000

    Lint / format

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
-import { print , prepareResourcePaths} from './utils/utils';
+import { print, prepareResourcePaths } from './utils/utils';
 import { config } from './config';
 import { MyWatcher } from './utils/watcher';
 
 print.info('TrueCore ResourceManager v1.0');
-print.debug("Resource paths: ", config.resources.paths);
+print.debug('Resource paths: ', config.resources.paths);
 
 const resourcePaths = prepareResourcePaths();
 
-const ServerWatcher = new MyWatcher( resourcePaths , config.watchOptions);
+const ServerWatcher = new MyWatcher(resourcePaths, config.watchOptions);
 ServerWatcher.startWatching();
-
-
-RegisterCommand('watch', () => {
-  console.log('test2')
-  // start watching for the resource
-},true)
